379af8f4892e1b88fcff24ce679db80ac1bd6c5b
Ankit Singh <ankit99403@gmail>
2023-09-17 23:59:22 +0530

    favicon added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Helmet>
+      <title>
+        TechGurus - Your Source for Tech Knowledge and Technology News
+      </title>
+      <meta
+        name="description"
+        content="TechGurus is a leading source for tech knowledge, technology news, and user advice. We provide you with unique insights and innovations in the field of technology."
+      />
+    </Helmet>
     <App />
   </React.StrictMode>
 );
